5f5e05373626d909fbe69aefd4a76f216a43f36d
marcbrathwaite <marcbrathwaite@gmail.com>
2018-11-28 18:03:57 -0500

    Configured SCSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <header className="App__header">
+          <h1>CurrencyPal</h1>
+        </header>
+
         
       </div>
     );
